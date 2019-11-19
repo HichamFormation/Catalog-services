01c373cd830b25c88627dbe89ab250863a76691d
faouzi.hicham90@gmail.com <faouzi.hicham90@gmail.com>
2019-11-19 16:37:24 +0100

    mvn clean

---
+++ src/main/java/org/cap/dao/ProductRepository.java
@@ -0,0 +1,11 @@
+
+package org.cap.dao;
+
+import org.cap.entities.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@RepositoryRestResource
+public interface ProductRepository extends MongoRepository<Product, String> {
+
+	
+}
