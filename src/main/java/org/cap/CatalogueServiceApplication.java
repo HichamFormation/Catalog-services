742f30357a0455340823e5ea496f82cbb0e28a2f
faouzi.hicham90@gmail.com <faouzi.hicham90@gmail.com>
2019-11-19 17:04:57 +0100

    modification de la classe commandLine Runner

--- src/main/java/org/cap/CatalogueServiceApplication.java
+++ src/main/java/org/cap/CatalogueServiceApplication.java
@@ -1,8 +1,5 @@
 package org.cap;
 
-import org.cap.dao.CategoryRepository;
-import org.cap.dao.ProductRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
